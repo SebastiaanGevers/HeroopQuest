d1ec476081a66624cfe12e9ac94b6c656e9f2c3b
Gevers <bas.gevers@hotmail.com>
2020-02-21 11:54:30 +0100

    ik heb helden met bug

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -7,6 +7,43 @@ namespace src_HeroopQuest.GameClasses
     enum HeldType { Barbaar, Dwerg, Elf, Tovenaar }
     class Held
     {
+        public Held(HeldType type,string naam)
+        {
+            Naam = naam;
+            HeldType = type;
+
+            switch (HeldType)
+            {
+                case HeldType.Barbaar:
+                    AanvalsDobbelstenen = 3;
+                    MaxInteligentie = 2;
+                    MaxLichaam = 8;
+                    break;
+                case HeldType.Dwerg:
+                    MaxInteligentie = 3;
+                    MaxInteligentie = 7;
+
+                    break;
+                case HeldType.Elf:
+                    MaxInteligentie = 4;
+                    MaxLichaam = 6;
+
+                    break;
+                case HeldType.Tovenaar:
+                    AanvalsDobbelstenen =1;
+                    MaxInteligentie = 6;
+                    MaxLichaam = 4;
+
+                    break;
+                default:
+                    break;
+            }
+            HuidigInteligentie = MaxLichaam;
+            HuidigLichaam = MaxLichaam;
+        }
+
+        
+
         public string  Naam { get; set; }
         public string Beschrijving { get;private set; }
         public int MaxInteligentie { get; private set; }
@@ -17,6 +54,10 @@ namespace src_HeroopQuest.GameClasses
         public int AanvalsDobbelstenen { get; private set; } = 2;
         public int VerdedegingsDobbbelstenen { get;private set; } = 2;
         public int LoopDobbelstenen { get; private set; } = 2;
+
+
+
+        
 
     }
 }
--- src_HeroopQuest/src_HeroopQuest/Program.cs
+++ src_HeroopQuest/src_HeroopQuest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using src_HeroopQuest.GameClasses;
+using System;
 
 namespace src_HeroopQuest
 {
@@ -6,7 +7,8 @@ namespace src_HeroopQuest
     {
         static void Main(string[] args)
         {
-         
+            Held held = new Held();
+
         }
     }
 }
